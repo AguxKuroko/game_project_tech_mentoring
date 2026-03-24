21d689d88ac231d8a65618f6bc196e628fa9ec22
Agnieszka <145929660+AguxKuroko@users.noreply.github.com>
2026-03-24 15:57:23 +0100

    openai first code

--- app/fast_api_endpoints.py
+++ app/fast_api_endpoints.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+from pathlib import Path
 
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, Request, status
+from fastapi.responses import FileResponse
 
 from app.models import RawgApiData
 from app.rawg_api import rawg_api_call
@@ -10,11 +12,12 @@ app = FastAPI()
 
 @app.get("/", include_in_schema=False)
 def home():
-    return "Worst Game of ....year!"
+    image_path = Path("app") / "home_endpoint_image" / "welcome.png"
+    return FileResponse(image_path)
 
 
 @app.get("/worst_game/{year}")
-def worst_game_per_year(year: int):
+def worst_game_per_year(year: int, request: Request):
     if year > datetime.now().year:  # if the year is in the future we do not fetch data"
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail=f"{year} is in the future."
@@ -23,6 +26,9 @@ def worst_game_per_year(year: int):
     worst_game: RawgApiData | None = rawg_api_call(year)
 
     if worst_game is not None:
+        if request.headers.get("x-secret") == "chaos":  # easter egg for more crazy memes
+            ...
+            # prompt += "Make it extremely cursed and broken."
         return worst_game  # fastapi will convert it to json
 
     # if year is <= current and no metacrtic -> error msg"
--- app/meme_generator.py
+++ app/meme_generator.py
@@ -1,12 +1,33 @@
+import base64
+
 from openai import OpenAI
 
 from app.api_keys_config import api_game_key
 
 client = OpenAI(api_key=api_game_key.OPEN_AI_API_KEY)
+prompt_for_openai = """Transform this photo into a stylized cartoon meme with a video game vibes.
+Place the dog clearly in the center as the main character.
+Make the style look like a slightly low-quality, humorous video game character,
+with exaggerated features and expressive emotions.
+Use vibrant but slightly distorted colors, like a buggy or poorly designed game.
+Add large, bold, sloppy meme-style text at the top:
+'WELCOME TO WORST GAME GENERATOR'
+Optionally add smaller subtitle text at the bottom:
+'Where every game is a mistake'
+Make the overall tone ironic and funny, like a parody of bad video games.
+Add subtle gaming elements (UI, glitch effects, pixelation, or fake HUD elements).Keep it playful,
+chaotic, and meme-like."""
 
-result = client.images.generate(
+result = client.images.edit(
     model="gpt-image-1",
-    prompt="A funny meme of a sad video game controller",
+    image=open("kuroko.jpg", "rb"),  # your base image
+    prompt=prompt_for_openai,
     size="1024x1024",
-    quality="low",
 )
+
+# Save result
+image_base64 = result.data[0].b64_json
+
+
+with open("output.png", "wb") as f:
+    f.write(base64.b64decode(image_base64))
