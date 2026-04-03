1f07cff4b360661ec1d38c9c295c5df25f46cb1c
Agnieszka <145929660+AguxKuroko@users.noreply.github.com>
2026-04-03 19:32:03 +0200

    db utlis

--- app/db/db_models.py
+++ app/db/db_models.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 
+from db_utils import get_time
 from sqlmodel import Field, SQLModel
 
 
 class Meme(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    game_name: str
     file_path: str
     year: int
-    created_at: datetime = Field(default_factory=datetime.timezone.utc)
+    created_at: datetime = Field(default_factory=get_time)
 
 
 class MemeStats(SQLModel, table=True):
---
+++ app/db/db_utils.py
@@ -0,0 +1,5 @@
+from datetime import UTC, datetime
+
+
+def get_time():
+    return datetime.now(UTC)
