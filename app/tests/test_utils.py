961b4546d2832f29dc33be2c3ffefa0d2e4e79c2
Agnieszka <145929660+AguxKuroko@users.noreply.github.com>
2026-04-22 22:48:51 +0200

    unit test utilis 4

--- app/tests/test_utils.py
+++ app/tests/test_utils.py
@@ -1,6 +1,12 @@
-import pytest
+from io import BytesIO
+from unittest.mock import Mock, patch
 
-from app.utils import extract_genres, extract_release_year, prepare_images_for_openai
+import pytest
+import requests
+from fastapi import HTTPException
+
+from app.models import RawgApiData
+from app.utils import build_prompt, clean_filename, extract_genres, extract_release_year, prepare_images_for_openai
 
 
 class TestExtractReleaseYear:
@@ -43,9 +49,71 @@ class TestExtractGenres:
 
 
 class TestPrepareImagesForOpenAi:
-    def empty_list(self):
+    def test_empty_list(self):
         data = []
         actual = prepare_images_for_openai(data)
         expected = []
 
         assert actual == expected
+
+
+class TestPrepareImagesForOpenAiMock:
+    @patch("app.utils.requests.get")
+    def test_prepare_image_requests(self, requests_mock):
+        mock_response = Mock(spec=requests.Response)
+        mock_response.content = b"fake image bytes"
+
+        requests_mock.return_value = mock_response
+
+        screenshot = ["http://test.com/image.jpg", "http://test.com/image_two.jpg"]
+
+        result = prepare_images_for_openai(screenshot)
+
+        assert len(result) == 2
+        assert isinstance(result[0], BytesIO)
+        assert result[1].name == "image_1.jpg"
+
+    @patch("app.utils.requests.get")
+    def test_download_failure(self, mock_get):
+        mock_get.side_effect = requests.exceptions.RequestException
+
+        with pytest.raises(HTTPException):
+            prepare_images_for_openai(["http://bad-url"])
+
+
+@pytest.fixture
+def rawg_api_fake_game():
+    return RawgApiData(
+        game_name="Unit test 2025",
+        game_id=1,
+        game_release_year="2021",
+        game_meta_score=20,
+        game_genre=["action", "fps"],
+        game_dropped_count=777,
+        game_screenhosts=[],
+    )
+
+
+class TestBuildPrompt:
+    def test_normal_mode(self, rawg_api_fake_game):
+        result = build_prompt(rawg_api_fake_game, "normal")
+
+        assert "Unit test 2025" in result
+        assert "2021" in result
+
+    def test_dog_mode(self, rawg_api_fake_game):
+        result = build_prompt(rawg_api_fake_game, "dog")
+
+        assert "Secret 'DOG' mode" in result
+        assert "action" in result
+
+
+class TestCleanFilename:
+    def test_remove_special_chars():
+        assert clean_filename("game@#2021!") == "game2021"
+
+    def test_spaces_to_underscore():
+        assert clean_filename("my game name") == "my_game_name"
+
+    def test_strip_spaces():
+        assert clean_filename("  test game  ") == "test_game"
