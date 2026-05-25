483fb070fd766bcac3c0f81e7789e02c67a6e6d8
Agnieszka <145929660+AguxKuroko@users.noreply.github.com>
2026-05-25 20:15:48 +0200

    Feat/logger (#11)
    * logger
    
    * logger 2
    
    * logger 3
    
    * logger 4
    
    * logger 5
    
    * logger 6
    
    * logger 7
    
    * logger 10

---
+++ app/logging_config.py
@@ -0,0 +1,33 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+import logfire
+
+from app.app_config import app_paths
+
+
+def setup_logging():
+    logger = logging.getLogger()
+
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+
+        formatter = logging.Formatter("%(asctime)s | %(name)s | %(levelname)s | %(message)s | %(filename)s:%(lineno)d (%(funcName)s)")
+
+        file_handler = RotatingFileHandler(
+            app_paths.logs_dir / "worst_game_meme_app.log",
+            maxBytes=1_000_000,
+            backupCount=3,
+        )
+        file_handler.setFormatter(formatter)
+
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+        logger.addHandler(logfire.LogfireLoggingHandler())
+
+        logging.getLogger("sqlalchemy.engine").setLevel(logging.WARNING)
+
+    logfire.configure(service_name="worst-game-meme-app")
